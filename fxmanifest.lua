197738e9ef4a03a97de6d03d0621251fbac59074
Dominic Rico Gómez <dominic.rico@sevdesk.de>
2021-11-27 14:54:15 +0100

    :rocket: folder structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,15 @@ game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
-  'client.lua',
-  'warmenu.lua',
+  'client/*.lua',
 }
 
 server_scripts {
-  'server.lua'
+  'server/*.lua'
 }
 
 shared_scripts {
-  'utils.lua'
+  'utils/*.lua'
   'config.lua',
   'locale.lua',
   'locales/*.lua',
